f9c35c4bdf0db59cf20f4d3ed65ed235ffc07b39
Estebanm1812 <69942961+Estebanm1812@users.noreply.github.com>
2021-04-23 16:41:44 -0500

    Classes created, Matrix creation methods added

---
+++ src/model/Node.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Node {
+
+	private int row;
+	private int column;
+	
+	private Node next;
+	private Node previous;
+	private Node up;
+	private Node down;
+	
+	public Node getNext() {
+		return next;
+	}
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	public Node getPrevious() {
+		return previous;
+	}
+	public void setPrevious(Node previous) {
+		this.previous = previous;
+	}
+	public Node getUp() {
+		return up;
+	}
+	public void setUp(Node up) {
+		this.up = up;
+	}
+	public Node getDown() {
+		return down;
+	}
+	public void setDown(Node down) {
+		this.down = down;
+	}
+	public Node(int row,int column) {
+		this.row = row;
+		this.column = column;
+	}
+	public int getRow() {
+		return row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	
+}
